2fbd565c8f2d98a4ae98b2457aeadee99df3bff4
kairox <git@newgenerations.im>
2018-09-24 20:39:33 +0900

    アナリティクス

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 import 'babel-polyfill'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-121406993-2',
+  router
+})
 
 /* eslint-disable no-new */
 new Vue({
