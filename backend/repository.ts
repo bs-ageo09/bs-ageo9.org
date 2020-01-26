ae2fb1f6a5e2584d2cf361a0c6799e163d013dbb
qazx7412 <git@newgenerations.im>
2020-01-27 01:42:36 +0900

    リポジトリを統合

---
+++ backend/repository.ts
@@ -0,0 +1,72 @@
+import { Events, Address, Paper } from './models'
+
+export class SpreadsheetRepository {
+  id: string
+
+  constructor(id: string) {
+    this.id = id
+  }
+
+  findEventsList(): Events[] {
+    const sheet = SpreadsheetApp.openById(this.id).getSheetByName('event')
+    const rows = sheet.getDataRange().getValues()
+    const keys = rows.splice(0, 1)[0]
+
+    return rows.map(row => {
+      let obj = {}
+      row.map((item, index) => {
+        obj[keys[index]] = item
+      })
+      Logger.log(obj)
+      return new Events(
+        obj['index'],
+        obj['is_information'],
+        obj['event_name'],
+        obj['heading'],
+        obj['date'],
+        obj['text'],
+        obj['doc_link'],
+        obj['doc_name']
+      )
+    })
+  }
+
+  findAddressList(): Address[] {
+    const sheet = SpreadsheetApp.openById(this.id).getSheetByName('address')
+    const rows = sheet.getDataRange().getValues()
+    const keys = rows.splice(0, 1)[0]
+
+    return rows.map(row => {
+      let obj = {}
+      row.map((item, index) => {
+        obj[keys[index]] = item
+      })
+      Logger.log(obj)
+      return new Address(
+        obj['rank'],
+        obj['name'],
+        obj['name_hira'],
+        obj['number'],
+        obj['type']
+      )
+    })
+  }
+
+  findPaperList(): Paper[] {
+    const sheet = SpreadsheetApp.openById(this.id).getSheetByName('paper')
+    const rows = sheet.getDataRange().getValues()
+    const keys = rows.splice(0, 1)[0]
+
+    return rows.map(row => {
+      let obj = {}
+      row.map((item, index) => {
+        obj[keys[index]] = item
+      })
+      Logger.log(obj)
+      return new Paper(
+        obj['link'],
+        obj['name']
+      )
+    })
+  }
+}
