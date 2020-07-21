e082a4f008c9ae10fffdc6d9b95612b0b6763699
qazx7412 <git@newgenerations.im>
2020-07-22 02:23:10 +0900

    英語表記の修正

--- components/layouts/appcopl.vue
+++ components/layouts/appcopl.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="appcopl" >
-    <p>Copyright 2017 Boy Scout 9thAgeo . All Rights Reserved.
+    <p>Copyright 2017 9th Ageo Group, Saitama Scout Council, SAJ. All Rights Reserved.
     </p>
     <p class="cut">このホームページは、設置者（上山芳範：ボーイスカウト埼玉県連盟上尾9団）＜ueyama362@gmail.com＞の責任の下で掲載するものであり、 ボーイスカウト日本連盟が情報を提供しているものではありません。
     </p>
